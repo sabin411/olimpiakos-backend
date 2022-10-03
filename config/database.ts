50747b59eeb88384e1f82c0776165c1d8dcae926
sabin-coresoftint <sabin@coresoftint.com>
2022-10-04 00:21:02 +0545

    OLYM initial commit.

--- config/database.ts
+++ config/database.ts
@@ -1,12 +1,18 @@
+const databaseHost = process.env.DATABASE_HOST || "localhost";
+const databasePort = process.env.DATABASE_PORT || 5432;
+const databaseName = process.env.DATABASE_NAME || "sabinshrestha";
+const databaseUser = process.env.DATABASE_USER || "postgres";
+const databasePassword = process.env.DATABASE_PASSWORD || "password";
+
 export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "sabinshrestha"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "password"),
+      host: env("DATABASE_HOST", databaseHost),
+      port: env.int("DATABASE_PORT", databasePort),
+      database: env("DATABASE_NAME", databaseName),
+      user: env("DATABASE_USERNAME", databaseUser),
+      password: env("DATABASE_PASSWORD", databasePassword),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
