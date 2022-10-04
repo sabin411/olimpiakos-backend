fc5d753450cca580de8e581650da84f24e40191c
sabin-coresoftint <sabin@coresoftint.com>
2022-10-04 15:29:57 +0545

    Update database config

--- dist/src/api/comment/controllers/comment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * comment controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::comment.comment');
--- dist/src/api/featured-video/services/featured-video.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * featured-video service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::featured-video.featured-video');
