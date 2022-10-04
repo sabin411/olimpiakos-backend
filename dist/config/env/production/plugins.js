d790c571b9bda3d6556c099df3dd2d903b8cc2c5
sabin-coresoftint <sabin@coresoftint.com>
2022-10-04 17:48:47 +0545

    increased depth limit by 20

--- dist/config/env/production/plugins.js
+++ dist/config/env/production/plugins.js
@@ -4,7 +4,7 @@ module.exports = {
             endpoint: "/graphql",
             shadowCRUD: true,
             playgroundAlways: true,
-            depthLimit: 10,
+            depthLimit: 30,
             amountLimit: 100,
             apolloServer: {
                 tracing: false,
